86b444f4c9a12a17a41b6a805ee4cac75004b63a
DevPatel-art <dev.patel@edu.sait.ca>
2024-11-06 17:02:51 -0700

    changes

---
+++ javascript.js
@@ -0,0 +1,59 @@
+sheep.addEventListener('click',function(){
+    console.log('sheep on mountain');
+    document.getElementById('image').src='sheep.webp';
+    document.getElementById('sheep').style.backgroundColor='#97bf04'
+    document.getElementById('wolf').style.backgroundColor='#d9043d'
+    document.getElementById('moose').style.backgroundColor='#d9043d'
+    document.getElementById('elk').style.backgroundColor='#d9043d'
+    document.getElementById('bear').style.backgroundColor='#d9043d'
+
+})
+
+bear.addEventListener('click',function(){
+    console.log('bear');
+    document.getElementById('image').src='bear.webp';
+    document.getElementById('bear').style.backgroundColor='#97bf04'
+    document.getElementById('wolf').style.backgroundColor='#d9043d'
+    document.getElementById('sheep').style.backgroundColor='#d9043d'
+    document.getElementById('moose').style.backgroundColor='#d9043d'
+    document.getElementById('elk').style.backgroundColor='#d9043d'
+})
+
+moose.addEventListener('click',function(){
+    console.log('moose');
+    document.getElementById('image').src='moose.webp';
+    document.getElementById('moose').style.backgroundColor='#97bf04'
+    document.getElementById('wolf').style.backgroundColor='#d9043d'
+    document.getElementById('sheep').style.backgroundColor='#d9043d'
+    document.getElementById('bear').style.backgroundColor='#d9043d'
+    document.getElementById('elk').style.backgroundColor='#d9043d'
+})
+
+wolf.addEventListener('click',function(){
+    console.log('wolf');
+    document.getElementById('image').src='wolf.webp';
+    document.getElementById('wolf').style.backgroundColor='#97bf04'
+    document.getElementById('bear').style.backgroundColor='#d9043d'
+    document.getElementById('sheep').style.backgroundColor='#d9043d'
+    document.getElementById('moose').style.backgroundColor='#d9043d'
+    document.getElementById('elk').style.backgroundColor='#d9043d'
+})
+
+elk.addEventListener('click',function(){
+    console.log('elk');
+    document.getElementById('image').src='elk.webp';
+    document.getElementById('elk').style.backgroundColor='#97bf04'
+    document.getElementById('wolf').style.backgroundColor='#d9043d'
+    document.getElementById('sheep').style.backgroundColor='#d9043d'
+    document.getElementById('moose').style.backgroundColor='#d9043d'
+    document.getElementById('bear').style.backgroundColor='#d9043d'
+})
+   
+
+
+
+
+
+
+
+
